b07e4e0ac6ec91666fb824481458850ff9653202
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-06-09 22:06:53 +0100

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 let popup = document.querySelector(".backdrop");
+let box = document.querySelectorAll(".box");
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
@@ -43,3 +44,24 @@ const sendEmail = (e) => {
   };
 
   form.addEventListener("submit", sendEmail)
+
+
+  box.forEach(input => {
+    input.addEventListener("focus", (e) => {
+      const span = e.target.previousElementSibling;
+      span.style.transform = "translateY(-5rem)";
+      span.style.fontSize = "12px";
+      console.log(span)
+    })
+
+  });
+
+  box.forEach(input => {
+    input.addEventListener("blur", (e) => {
+      if(input.value.length > 0) return;
+      const span = e.target.previousElementSibling;
+      span.style = true;
+      console.log(span)
+    })
+
+  });
